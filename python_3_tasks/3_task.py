90897933f4d0fe730ec0f7541e195036c46f24b0
TymurD <89219322+TymurD@users.noreply.github.com>
2024-05-13 11:57:53 +0300

    Delete python_3_tasks directory

--- python_3_tasks/3_task.py
+++
@@ -1,6 +0,0 @@
-while(True):
-    money = int(input("how much money do you have? "))
-    if money <= 500 and money >= 100 or money <= 5000 and money >= 1000:
-        print("yes")
-    else:
-        print("no")
--- python_3_tasks/4_task.py
+++
@@ -1,8 +0,0 @@
-while(True):
-    ninja = int(input("amount oj ninjas "))
-    if ninja <= 10:
-        print("i can beat those")
-    elif ninja <= 30:
-        print("hard, but possible")
-    elif ninja > 30:
-        print("too much")
