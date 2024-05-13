2ef7a545317db4e4eb0ce93a84716e7d3e807020
TymurD <89219322+TymurD@users.noreply.github.com>
2024-05-13 11:57:46 +0300

    Delete python_2_tasks directory

--- python_2_tasks/1_task.py
+++
@@ -1,8 +0,0 @@
-import turtle
-t = turtle
-for i in range(0,2):
-    t.forward(50)
-    t.left(90)
-    t.forward(30)
-    t.left(90)
-input()
--- python_2_tasks/2_task.py
+++
@@ -1,6 +0,0 @@
-import turtle
-t = turtle
-for i in range(0,3):
-    t.left(120)
-    t.forward(50)
-input()
